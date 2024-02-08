783337311ba0890d3c8ec31f6db666d9d99a2ca1
300345648 <chau20nm@gmail.com>
2024-02-08 10:32:29 -0800

    444

--- app/src/main/java/com/example/week6project/MainActivity.java
+++ app/src/main/java/com/example/week6project/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 //Added another Line from local on Feb 8
     //Third change
+    //dfdg
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
